833cc3f9d9cce4a281d1a63ba7773795e09fca19
Esti Gajda <egajda@umich.edu>
2021-04-07 14:10:01 -0400

    Move function overloading to run time

--- src/HydrophoneCalibrations.jl
+++ src/HydrophoneCalibrations.jl
@@ -14,20 +14,30 @@ end
 
 calibration(::Val{:empty}) = []
 
-function __init__()
-    absolute_include(file) = include(joinpath(pwd(), file))
+const data_loaded = Ref{Bool}()
+function load_config()
+    if !data_loaded[]
+        @info pwd()
+        absolute_include(file) = include(joinpath(pwd(), file))
 
-    if isfile("calibration/config.jl")
-        absolute_include("calibration/config.jl")
-    elseif isfile("../calibration/config.jl")
-        absolute_include("../calibration/config.jl")
-    else
-        @info "No calibration folder found"
+        if isfile("calibration/config.jl")
+            absolute_include("calibration/config.jl")
+        elseif isfile("../calibration/config.jl")
+            absolute_include("../calibration/config.jl")
+        else
+            @info "No calibration folder found"
+        end
+        data_loaded[] = true
     end
 end
 
+function __init__()
+end
+
 function get_calibration(hydro_id, preamp_id = :empty)
-    return calibration(Val(hydro_id)), calibration(Val(preamp_id))
+    load_config()
+    call_calibration(s) = Base.invokelatest(calibration, Val(s))
+    return call_calibration(hydro_id), call_calibration(preamp_id)
 end
 
 include("volt_to_pressure.jl")
